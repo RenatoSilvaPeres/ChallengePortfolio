6a0563fc05df1705a36544c91e0cf33c2de92e81
Renato Peres <otaner_pc1@hotmail.com>
2022-07-04 02:46:31 -0300

    reparos gerais e criação de um arquivo js

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+var sobreMim = document.querySelector(".sobre");
+var skills = document.querySelector(".habilidades");
+var formacao = document.querySelector(".formacao");
+var projetos = document.querySelector(".experiencia");
+
+
