0c478caa46f6ffc19ea863a026f0f7e8afaf0b6d
Renato Peres <otaner_pc1@hotmail.com>
2022-07-11 17:45:12 -0300

    adicionado imagens sessão skill e consertado bugs responsividade

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,47 @@
-var sobreMim = document.querySelector(".sobre");
-var skills = document.querySelector(".habilidades");
-var formacao = document.querySelector(".formacao");
-var projetos = document.querySelector(".experiencia");
+var sobreMim = document.getElementById("sobre");
+var skills = document.getElementById("habilidades");
+var formacao = document.getElementById("formacao");
+var projetos = document.getElementById("experiencia");
+var chamada = document.getElementById ("chamada");
+var lista = document.getElementById ("cabecalho__menu-lista");
 
 
+lista.addEventListener ("click", function (evento){
+  var eventoTarget = evento.target;
+ verificaDataset(eventoTarget);
+
+})
+function verificaDataset (evento){
+  const dates = evento.dataset.tipo
+  if(funcoes[dates]){
+    funcoes[dates]()
+  }
+}
+var funcoes = {
+  habilidades: function(){
+   tiraDisplay(skills);
+  },
+  sobre: function(){
+    tiraDisplay(sobreMim);
+  },
+  home: function(){
+    location.reload();
+  },
+  formacao: function(){
+    tiraDisplay(formacao);
+    
+  },
+  projetos: function(){
+    tiraDisplay(projetos);
+  }
+
+}
+
+function tiraDisplay(sectionComDisplay){
+  sobreMim.style.display = "none";
+  skills.style.display = "none";
+  formacao.style.display = "none";
+  projetos.style.display = "none";
+  chamada.style.display = "none";
+  sectionComDisplay.style.display = "flex";
+}
